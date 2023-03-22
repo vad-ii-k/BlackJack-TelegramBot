6f9f4e39d43401d4d9b8f739918448966594268b
vad-ii-k <sazanov7615@gmail.com>
2023-03-23 00:22:06 +0300

    added docs/player route

--- app/web/routes.py
+++ app/web/routes.py
@@ -3,5 +3,9 @@ from aiohttp.web_app import Application
 
 def setup_routes(app: Application):
     from app.admin.routes import setup_routes as admin_setup_routes
+    from app.chat.routes import setup_routes as chat_setup_routes
+    from app.player.routes import setup_routes as player_setup_routes
 
     admin_setup_routes(app)
+    player_setup_routes(app)
+    chat_setup_routes(app)
